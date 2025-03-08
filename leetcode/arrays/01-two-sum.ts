d3cae166a6546b07b2ff512ce0c04d46c329913e
Bismark Agyare <bismarkagyare54@gmail.com>
2025-03-08 14:11:41 +0000

    two sum optimised solution

--- leetcode/arrays/01-two-sum.ts
+++ leetcode/arrays/01-two-sum.ts
@@ -5,13 +5,32 @@ function twoSum(nums: number[], target: number) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
-        return [i, j]
+        return [i, j];
       }
     }
   }
 
-  return []
+  return [];
 }
 
-const result = twoSum([4, 3, 2], 6)
-console.log(result)
+function twoSumOptimised(nums: number[], target: number) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let sum = nums[left] + nums[right];
+
+    if (sum === target) {
+      return [nums[left], nums[right]];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return [];
+}
+
+const result = twoSumOptimised([1, 2, 3, 4, 5], 3);
+console.log(result);
