ba26a9833ff431c7742fbc3368ce6fbb2c0150f1
Bismark Agyare <bismarkagyare54@gmail.com>
2025-03-11 11:07:26 +0000

    optimised solution

---
+++ leetcode/arrays/217-contains-duplicates.ts
@@ -0,0 +1,34 @@
+//brute force approach : time complexity of O(n^2)
+
+function containsDuplicate(nums: number[]) {
+  if (nums.length === 0) return false;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+function containsDuplicateOptimised(nums: number[]) {
+  if (nums.length === 0) return false;
+
+  let set = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return true;
+    }
+
+    set.add(nums[i]);
+  }
+
+  return false;
+}
+
+const solution = containsDuplicateOptimised([1, 2, 3, 4, 1]);
+console.log(solution);
