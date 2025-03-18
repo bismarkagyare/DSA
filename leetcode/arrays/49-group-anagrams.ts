804818e9201056df57ce5f23f01911af27aeb043
Bismark Agyare <bismarkagyare54@gmail.com>
2025-03-18 19:45:37 +0000

    implement groupAnagrams function to group anagrams from the input array and return them as an array of arrays

--- leetcode/arrays/49-group-anagrams.ts
+++ leetcode/arrays/49-group-anagrams.ts
@@ -4,6 +4,23 @@
 
 //Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
-function groupAnagrams(strs: string[]) {
-  
-};
+function groupAnagrams(strs: string[]) : string[][] {
+  const hashMap = new Map<string, string[]>();
+
+  for (let word of strs) {
+    const key = word.split("").sort().join("");
+
+    //check if the key exists
+    if (hashMap.has(key)) {
+      // if the key exists, push the current word into the existing arr
+      hashMap.get(key)?.push(word);
+    } else {
+      hashMap.set(key, [word]);
+    }
+  }
+
+  return Array.from(hashMap.values());
+}
+
+const solution = groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]);
+console.log(solution);
