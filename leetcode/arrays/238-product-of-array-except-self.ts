7c9b32f890db17dc178cf5fcc1c20782a08230c0
Bismark Agyare <bismarkagyare54@gmail.com>
2025-03-27 20:51:34 +0000

    first attempt

---
+++ leetcode/arrays/238-product-of-array-except-self.ts
@@ -0,0 +1,19 @@
+//Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+function productExceptSelf(nums: number[]) {
+  const answer = Array(nums.length).fill(1)
+  let leftProduct = 1
+  let rightProduct = 1
+  
+  for (let i = 0; i < answer.length; i++) {
+    answer[i] = leftProduct;
+    leftProduct = leftProduct * nums[i]
+  }
+
+  
+
+  return answer
+}
+
+const sol = productExceptSelf([1, 2, 3, 4])
+console.log("ans in function call", sol)
