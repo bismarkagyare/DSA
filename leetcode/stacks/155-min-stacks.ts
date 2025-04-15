23d84650f64812c98c7657aa33b812c8b2511144
Bismark Agyare <bismarkagyare54@gmail.com>
2025-04-15 08:50:16 +0000

    solve problem 155

--- leetcode/stacks/155-min-stacks.ts
+++ leetcode/stacks/155-min-stacks.ts
@@ -9,3 +9,37 @@
 //int getMin() retrieves the minimum element in the stack.
 //You must implement a solution with O(1) time complexity for each function.
 
+class MinStack {
+  private stack: number[]
+  private minStack: number[]
+
+  constructor() {
+    this.stack = []
+    this.minStack = []
+  }
+
+  push(val: number): void {
+    this.stack.push(val)
+
+    if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
+      this.minStack.push(val)
+    }
+  }
+
+  pop(): void {
+    const removed = this.stack.pop()
+
+    if (removed === this.minStack[this.minStack.length - 1]) {
+      this.minStack.pop()
+    }
+  }
+
+  top(): number {
+    return this.stack[this.stack.length - 1]
+  }
+
+  getMin(): number {
+    return this.minStack[this.minStack.length - 1]
+  }
+
+}
