248ce535c33caf9c447c3c9e6e04e684854f2d03
Bismark Agyare <bismarkagyare54@gmail.com>
2025-04-17 13:11:38 +0000

    739 daily temps solved

--- leetcode/stacks/739-daily-temperatures.ts
+++ leetcode/stacks/739-daily-temperatures.ts
@@ -1,3 +1,21 @@
 function dailyTemperatures(temperatures: number[]): number[] {
+  const answer: number[] = new Array(temperatures.length).fill(0)
   
+  const stack: number[] = []
+
+  for (let currentDay = 0; currentDay < temperatures.length; currentDay++) {
+    const currentTemp = temperatures[currentDay]
+
+    while (stack.length > 0 && currentTemp > temperatures[stack[stack.length - 1]]) {
+      const previousDay = stack.pop()!
+      answer[previousDay] = currentDay - previousDay
+    }
+
+    stack.push(currentDay)
+  }
+
+  return answer
 };
+
+const sl = dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])
+console.log(sl)
