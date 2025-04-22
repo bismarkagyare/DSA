b50591c5c97aee701d1ee335ffbd13893130d49c
Bismark Agyare <bismarkagyare54@gmail.com>
2025-04-22 12:54:51 +0000

    optimised solution

--- leetcode/binary-search/74-search-2D-matrix.ts
+++ leetcode/binary-search/74-search-2D-matrix.ts
@@ -28,5 +28,29 @@ function searchMatrix(matrix: number[][], target: number): boolean{
   return false
 };
 
+function searchMatrixOptimised(matrix: number[][], target: number): boolean {
+  if (!matrix.length || !matrix[0].length) return false;
+
+  const m = matrix.length;
+  const n = matrix[0].length;
+
+  let start = 0;
+  let end = m * n - 1;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    const row = Math.floor(mid / n);
+    const col = mid % n;
+    const midValue = matrix[row][col];
+
+    if (midValue === target) return true;
+    else if (midValue < target) start = mid + 1;
+    else end = mid - 1;
+  }
+
+  return false;
+}
+
+
 const z = searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 23)
 console.log(z)
