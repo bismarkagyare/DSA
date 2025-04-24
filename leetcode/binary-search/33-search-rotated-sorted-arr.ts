7a40390f75e18ecacd3e890e95c65084436f558d
Bismark Agyare <bismarkagyare54@gmail.com>
2025-04-24 16:49:23 +0000

    solve leetcode 33

---
+++ leetcode/binary-search/33-search-rotated-sorted-arr.ts
@@ -0,0 +1,29 @@
+function search(nums: number[], target: number): number {
+  let left = 0
+  let right = nums.length - 1
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+
+    if (nums[mid]  === target) {
+      return mid
+    } else if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target <= nums[mid]) {
+        right = mid - 1
+      } else {
+        left = mid + 1
+      }
+    } else {
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1
+      } else {
+        right = mid - 1
+      }
+    }
+  }
+
+  return -1
+}
+
+const ans1 = search([4, 5, 6, 7, 0, 1, 2], 2);
+console.log(ans1);
