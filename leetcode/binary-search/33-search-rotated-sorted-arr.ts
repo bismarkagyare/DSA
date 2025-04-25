b806e0c47b896387070c0012b2c3dc0fc83f0d87
Bismark Agyare <bismarkagyare54@gmail.com>
2025-04-25 09:49:01 +0000

    clean up

--- leetcode/binary-search/33-search-rotated-sorted-arr.ts
+++ leetcode/binary-search/33-search-rotated-sorted-arr.ts
@@ -1,29 +1,48 @@
 function search(nums: number[], target: number): number {
-  let left = 0
-  let right = nums.length - 1
+  let left = 0;
+  let right = nums.length - 1;
 
   while (left <= right) {
-    const mid = Math.floor((left + right) / 2)
+    const mid = Math.floor((left + right) / 2);
 
-    if (nums[mid]  === target) {
-      return mid
-    } else if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target <= nums[mid]) {
-        right = mid - 1
-      } else {
-        left = mid + 1
-      }
+    if (nums[mid] === target) return mid;
+
+    const leftSorted = nums[left] <= nums[mid];
+
+    if (leftSorted) {
+      const inLeftRange = nums[left] <= target && target <= nums[mid];
+      if (inLeftRange) right = mid - 1;
+      else left = mid + 1;
     } else {
-      if (nums[mid] < target && target <= nums[right]) {
-        left = mid + 1
-      } else {
-        right = mid - 1
-      }
+      const inRightRange = nums[mid] < target && target <= nums[right];
+      if (inRightRange) left = mid + 1;
+      else right = mid - 1;
     }
+
   }
 
   return -1
+
 }
+
 
 const ans1 = search([4, 5, 6, 7, 0, 1, 2], 2);
 console.log(ans1);
+
+  //   if (nums[mid]  === target) {
+  //     return mid
+  //   } else if (nums[left] <= nums[mid]) {
+  //     if (nums[left] <= target && target <= nums[mid]) {
+  //       right = mid - 1
+  //     } else {
+  //       left = mid + 1
+  //     }
+  //   } else {
+  //     if (nums[mid] < target && target <= nums[right]) {
+  //       left = mid + 1
+  //     } else {
+  //       right = mid - 1
+  //     }
+  //   }
+  // }
+
