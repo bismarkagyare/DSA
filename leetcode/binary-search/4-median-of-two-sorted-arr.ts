c6390228c376d34b89e597d29e8c35f301146147
Bismark Agyare <bismarkagyare54@gmail.com>
2025-04-28 21:58:53 +0000

    attempt

--- leetcode/binary-search/4-median-of-two-sorted-arr.ts
+++ leetcode/binary-search/4-median-of-two-sorted-arr.ts
@@ -1,3 +1,14 @@
-function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-  
+function findMedianSortedArrays(nums1: number[], nums2: number[]) {
+  const merged = [...nums1, ...nums2]
+  let left = 0
+  let right = merged.length - 1
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+    
+  }
+  return merged
 };
+
+const r = findMedianSortedArrays([1, 2, 3], [4, 5])
+console.log(r)
