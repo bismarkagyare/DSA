71db1a9d83bf8b4984c14cfd0aa785b6584d7b22
Bismark Agyare <bismarkagyare54@gmail.com>
2025-05-02 10:18:46 +0000

    brute force approach

--- leetcode/binary-search/4-median-of-two-sorted-arr.ts
+++ leetcode/binary-search/4-median-of-two-sorted-arr.ts
@@ -1,14 +1,19 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]) {
   const merged = [...nums1, ...nums2]
-  let left = 0
-  let right = merged.length - 1
+  merged.sort((a, b) => a - b)
 
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2)
-    
+  const n = merged.length
+
+  if (n % 2 !== 0) {
+    //the length is odd
+    return merged[Math.floor(n / 2)]
+  } else {
+    //the length is even
+    const mid1 = n / 2 - 1
+    const mid2 = n / 2
+    return (merged[mid1] + merged[mid2]) / 2
   }
-  return merged
 };
 
-const r = findMedianSortedArrays([1, 2, 3], [4, 5])
+const r = findMedianSortedArrays([1, 2, 3], [4, 5, 6])
 console.log(r)
