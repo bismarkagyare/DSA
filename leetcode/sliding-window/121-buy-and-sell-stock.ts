9a88a8cb66d57e38ed8ceb47cb8e12c2055d6683
Bismark Agyare <bismarkagyare54@gmail.com>
2025-05-02 13:21:44 +0000

    leetcode problem 121

---
+++ leetcode/sliding-window/121-buy-and-sell-stock.ts
@@ -0,0 +1,28 @@
+//You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+//You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+//Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+function maxProfit(prices: number[]): number {
+  let maxProfit = 0;
+  let minPrice = Infinity;
+
+  for (let price of prices) {
+    if (price < minPrice) {
+      minPrice = price;
+    }
+
+    const currentProfit = price - minPrice;
+    if (currentProfit > maxProfit) {
+      maxProfit = currentProfit;
+    }
+  }
+
+  return maxProfit;
+}
+
+const max = maxProfit([7, 1, 5, 3, 6, 4]);
+console.log(max);
+
+//[1, 2, 3, 4, 5]
