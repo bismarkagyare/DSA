8a3c78bfb6fe30937f89715ec59365b7c2a88e69
Bismark Agyare <bismarkagyare54@gmail.com>
2025-05-09 16:34:34 +0000

    solve problem

---
+++ leetcode/sliding-window/3-longest-substring.ts
@@ -0,0 +1,23 @@
+function lengthOfLongestSubstring(s: string): number {
+  let left = 0
+  let maxLength = 0
+  const seen = new Set<string>()
+
+  for (let right = 0; right < s.length; right++) {
+    const currentChar = s[right]
+
+    while(seen.has(currentChar)) {
+      seen.delete(s[left])
+      left++
+    }
+
+    seen.add(currentChar)
+
+    maxLength = Math.max(maxLength, right - left + 1)
+  }
+
+  return maxLength
+};
+
+const sub = lengthOfLongestSubstring("abcabcbb")
+console.log(sub)
