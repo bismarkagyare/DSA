1329a76ae6c8690137c735f6658fe81a715a4c00
Bismark Agyare <bismarkagyare54@gmail.com>
2025-05-21 10:40:29 +0000

    use Map approach

--- leetcode/sliding-window/567-permutation-in-string.ts
+++ leetcode/sliding-window/567-permutation-in-string.ts
@@ -1,17 +1,21 @@
 //Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
 
-function checkInclusion(s1: string, s2?: string) {
-  const left = 0
-  const frequencyObj: Record<string, number> = {}
+function checkInclusion(s1: string, s2: string) {
+  if (s1.length > s2.length) return false
 
-  for (let right = 0; right < s1.length; right++) {
-    const char = s1[right]
-    frequencyObj[char] = (frequencyObj[char] || 0) + 1
+  const s1Map = new Map<string, number>()
+  const windowMap = new Map<string, number>()
+
+  //frequency map for s1
+  for (const char of s1) {
+    s1Map.set(char, (s1Map.get(char) || 0) + 1)
   }
+
+  let left = 0
 
 };
 
-const perm = checkInclusion("aabbccc")
+const perm = checkInclusion("ab", "eidbaooo")
 console.log(perm)
 
 
