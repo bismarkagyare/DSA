ddc756d7d6d014f1f5b62a3dd4866f6312612862
Bismark Agyare <bismarkagyare54@gmail.com>
2025-05-21 09:14:43 +0000

    get frequency count

--- leetcode/sliding-window/567-permutation-in-string.ts
+++ leetcode/sliding-window/567-permutation-in-string.ts
@@ -1,2 +1,17 @@
 //Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
 
+function checkInclusion(s1: string, s2?: string) {
+  const left = 0
+  const frequencyObj: Record<string, number> = {}
+
+  for (let right = 0; right < s1.length; right++) {
+    const char = s1[right]
+    frequencyObj[char] = (frequencyObj[char] || 0) + 1
+  }
+
+};
+
+const perm = checkInclusion("aabbccc")
+console.log(perm)
+
+
