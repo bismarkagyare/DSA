48cee384419e38cd7ff37df66d14d89f934a6c55
Bismark Agyare <bismarkagyare54@gmail.com>
2025-05-26 11:30:17 +0000

    add ListNode class

---
+++ leetcode/linkedlist/206-reverse-linkedlist.ts
@@ -0,0 +1,27 @@
+//Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+class ListNode {
+  val: number;
+  next: ListNode | null
+
+  constructor(val?:number, next?:ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
+}
+
+// function reverseList(head: ListNode | null): ListNode | null {
+//   let prev: ListNode | null = null
+//   let curr: ListNode | null = head
+
+//   while (curr !== null) {
+//     let next: ListNode | null = curr.next
+//     curr.next = prev
+//     prev = curr
+//     curr = next
+//   }
+
+//   return prev
+// };
+
+// type StoryProps = ComponentProps<typeof Button>
